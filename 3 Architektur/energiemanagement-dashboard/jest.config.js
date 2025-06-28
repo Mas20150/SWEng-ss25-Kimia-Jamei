b18901037c409605dc445a4a462c80a36705857b
Kimia Jamei <kimia.jamei@students.hs-mainz.de>
2025-06-28 18:56:05 +0200

    CI Pipline Konfiguration

--- 3 Architektur/energiemanagement-dashboard/jest.config.js
+++ 3 Architektur/energiemanagement-dashboard/jest.config.js
@@ -1,7 +1,7 @@
 /** @type {import('jest').Config} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   testMatch: ["**/Test/**/*.test.ts"],
   transform: {
     "^.+\\.tsx?$": "ts-jest"
